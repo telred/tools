0a1db8427a4d91d0b0f727cb50912ceb8da9335d
Evgenii B <evgenii at tel dot red>
2016-12-14 04:50:17 +0300

    License header updated

--- rinstall.sh
+++ rinstall.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
+# vim: set ts=2 sw=2 sts=2 et fenc=utf-8 :
+##########
+# Copyright (c) 2016, TEL.RED LLC,
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##########
+# Usage:
+#  $ curl https://${TELRED_REPOSITORY_BASE}/install.sh | sudo bash
+# or,
+#  $ wget https://${TELRED_REPOSITORY_BASE}/install.sh -O - | sudo bash
+# whichever suits you best
+##########
+
 set -euo pipefail
 
 TELRED_REPOSITORY_BASE='https://tel.red/repos'
